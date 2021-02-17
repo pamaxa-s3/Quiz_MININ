90b6ef4eec353e0170971ac54c2851882d06b4d1
Roman Strynzha <pamaxas3@gmail.com>
2021-02-17 22:44:42 +0200

    start project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Quiz from './containers/Quiz/Quiz'
+import Layout from './hoc/Layout/Layout'
+
+function App() {
+  return (
+    <div className="App">Hello world</div>
+  );
+}
+
+export default App;
